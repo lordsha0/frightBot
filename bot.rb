595e3b419c33e241df0a5c5fcebc706eb0955d4b
sha0 <lordsha0@protonmail.ch>
2021-12-09 06:24:10 -0500

    added watchlist command

--- bot.rb
+++ bot.rb
@@ -15,9 +15,12 @@ bot.command :witch do |action|
     action.respond("here, you lofi witch: https://www.youtube.com/watch?v=4Hg1Kudd_x4")
 end
 
-#  command
-bot.command :insertHere do |action|
-    action.respond()
+# watch list command
+bot.command :watchlist do |action|
+    action.respond("""```
+        to watch:
+        - The Witches 1990
+         ```""")
 end
 
 # basic help command
